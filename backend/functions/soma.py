acc3254799bf5c0042ed77c74e14498a88e39345
Jonatas Mathias Dalló <jonatasmathiasdallo@gmail.com>
2026-02-12 16:06:32 -0300

    README explicativo

---
+++ backend/functions/soma.py
@@ -0,0 +1,8 @@
+def somar(numero1, numero2):
+    print("==========================================")
+    print(f"Somando {numero1} e {numero2}")
+    print(f"Resultado: {numero1 + numero2}")
+    print("Operação realizada com sucesso.")
+    print("==========================================")
+
+    return numero1 + numero2
